1c099582c817c84c53564bef96fb5e15e1364493
fang-ren <ren.fang.ren@gmail.com>
2017-05-10 14:56:22 -0700

    added pilatus_detector branch

--- scripts/on_the_fly_assessment/image_loader.py
+++ scripts/on_the_fly_assessment/image_loader.py
@@ -3,14 +3,12 @@ author: Fang Ren (SSRL)
 
 4/27/2017
 """
-from PIL import Image
+
 import numpy as np
 
 
 def load_image(imageFullname):
     # open MARCCD tiff image
-    im = Image.open(imageFullname)
-    # input image object into an array
-    imArray = np.array(im)
-    im.close()
+    data = np.fromfile(imageFullname, dtype=np.int32)
+    imArray = data.reshape(195, 1475)
     return imArray
